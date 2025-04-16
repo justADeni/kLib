606f0e963a60ad3fc386c289c74d3c573bf95fa3
justADeni <deninikdo@gmail.com>
2025-04-17 00:05:28 +0200

    added jitpack.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.zorbeytorunoglu"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
@@ -41,7 +41,6 @@ tasks {
 
 tasks.shadowJar {
     archiveClassifier.set("")
-    transform(ServiceFileTransformer::class.java)
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
